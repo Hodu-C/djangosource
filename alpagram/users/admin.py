a20a1fc413c8b719cdb10c7a7402d1b7b563f6b9
removedporn <86824510+removedporn@users.noreply.github.com>
2023-07-21 19:45:52 +0800

    Delete alpagram directory

--- alpagram/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import CustomUser,Profile
-
-class CustomAdmin(admin.ModelAdmin):
-    list_display = ("email", "name", "nickname")
-
-admin.site.register(CustomUser,CustomAdmin)
-admin.site.register(Profile)
